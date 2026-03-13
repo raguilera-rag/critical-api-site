cf497f7fee4306ef3007870afc673a08242337de
Raul Aguilera <raguilera.hernandez.com>
2026-03-13 10:07:21 -0300

    fix sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://criticalapiservices.com',
-  integrations: [sitemap()]
+  integrations: [sitemap({})]
 });
